7f44e8a90985675c399e33a0f7eeb980a1f02006
Jan Timar <timar@cngroup.dk>
2020-12-04 00:57:06 +0100

    Add comments
    Signed-off-by: Marco Espinoza <marco_a_espinoza@yahoo.com>

--- Native-Frameworks/Screens/FrameWorkGridViewModel.swift
+++ Native-Frameworks/Screens/FrameWorkGridViewModel.swift
@@ -1,13 +1,15 @@
 import SwiftUI
 
 final class FrameworkGridViewModel: ObservableObject {
-    
+
+    // TODO: I think `selectedFramework` and `isShowingDetailView` can be join to one property `Freamwork` of `@Published` type and check if this value is nil ( I am not sure it will be work on 100% )
     var selectedFramework: Framework? {
         didSet { isShowingDetailView = true }
     }
     
     @Published var isShowingDetailView = false
-    
+
+    // TODO: You dont need specify type of your array
     let columns: [GridItem] = [GridItem(.flexible()),
                                GridItem(.flexible()),
                                GridItem(.flexible())]
--- Native-Frameworks/Screens/FrameworkGridView.swift
+++ Native-Frameworks/Screens/FrameworkGridView.swift
@@ -1,7 +1,8 @@
 import SwiftUI
 
 struct FrameworkGridView: View {
-    
+
+    // TODO: Probably this property can be private
     @StateObject var viewModel = FrameworkGridViewModel()
     @State private var isPressed = false
     @State private var isGridviewSelected = true
@@ -23,18 +24,22 @@ struct FrameworkGridView: View {
                     .navigationTitle("🍎 Frameworks")
                     .navigationBarItems(trailing: EllipsisButton(isPressed: $isPressed))
                     .fullScreenCover(isPresented: $viewModel.isShowingDetailView) {
-                        FrameworkDetailView(isShowingDetailView: $viewModel.isShowingDetailView, framework: viewModel.selectedFramework ?? MockData.sampleFramework)
+                        FrameworkDetailView(
+                            isShowingDetailView: $viewModel.isShowingDetailView,
+                            framework: viewModel.selectedFramework ?? MockData.sampleFramework // TODO: Really you want as default value `MockData` ? and if yes, it should be in your ViewModel
+                        )
                     }
                 }
                 
             }
             //TODO: Refactor logic to viewModel
+            // Note: Its view and it don't and I dont see any extra logic, but probably you can move it to separete property
             if isPressed {
                 GeometryReader { geo in
                     PopupSelectionView(isGridviewSelected: $isGridviewSelected, isListviewSelected: $isListviewSelected)
                 }
                 .offset(x: UIScreen.screenWidth * 0.61, y: UIScreen.screenHeight * 0.1)
-                .background(Color.black.opacity(0.65))
+                .background(Color.black.opacity(0.65)) // TODO: Colors like this will be probably on many places in your app, you should move it on one plalce
                 .edgesIgnoringSafeArea(.all)
                 .onTapGesture {
                     withAnimation {
@@ -53,6 +58,7 @@ struct FrameworkGridView_Previews: PreviewProvider {
     }
 }
 
+// TODO: If it is only one one place, plase make it private, I think this can be used on more places in app and it should be in separete file
 struct EllipsisButton: View {
     
     @Binding var isPressed: Bool
@@ -63,7 +69,7 @@ struct EllipsisButton: View {
                 self.isPressed.toggle()
             }
         } label: {
-            Image(systemName: "ellipsis")
+            Image(systemName: "ellipsis") // TODO: Stuff like image system names can be in one `enum`
                 .resizable()
                 .scaledToFit()
                 .frame(width: 30, height: 30)
@@ -74,6 +80,7 @@ struct EllipsisButton: View {
     }
 }
 
+// TODO: Can you replace this constants with `GeometryReader`
 extension UIScreen {
     static let screenWidth = UIScreen.main.bounds.size.width
     static let screenHeight = UIScreen.main.bounds.size.height
